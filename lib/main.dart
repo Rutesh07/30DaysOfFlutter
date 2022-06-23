9f9fda7fda7f5d2a2f6508547e310f36c643ad21
Rutesh Zalavadiya <20dit105@charusat.edu.in>
2022-06-23 23:50:21 +0530

    day12 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:practice1/pages/login_page.dart';
 import 'package:practice1/utils/routes.dart';
 import 'package:practice1/widgets/themes.dart';
--- lib/pages/Home_Page.dart
+++ lib/pages/Home_Page.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
-
-import '../widgets/drawer.dart';
+import 'package:practice1/models/catalog.dart';
+import 'package:practice1/widgets/drawer.dart';
+import 'package:practice1/widgets/item_widget.dart';
 
 class HomePage extends StatelessWidget {
   final int days = 30;
   final String name = "Rutesh";
   @override
   Widget build(BuildContext context) {
+    final dummyList = List.generate(20, (index) => CatalogModel.items[0]);
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
@@ -15,9 +17,15 @@ class HomePage extends StatelessWidget {
           style: TextStyle(color: Colors.black),
         ),
       ),
-      body: Center(
-        child: Container(
-          child: Text("Welcome To $days Days of Flutter With $name "),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView.builder(
+          itemCount: dummyList.length,
+          itemBuilder: (context, index) {
+            return ItemWidget(
+              item: dummyList[index],
+            );
+          },
         ),
       ),
       drawer: MyDrawer(),
