57649b728f2bbb54a3344cc0c5a327f0ee18384e
Rutesh Zalavadiya <20dit105@charusat.edu.in>
2022-06-25 00:39:23 +0530

    Day 17 completed

--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -1,22 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class MyTheme{
-  static ThemeData  lightTheme(BuildContext context) =>ThemeData(
-          primarySwatch: Colors.brown,
-          fontFamily: GoogleFonts.lato().fontFamily,
-          appBarTheme: AppBarTheme(
-            color: Colors.white,
-            elevation: 0.0,
-            textTheme: Theme.of(context).textTheme,
-            iconTheme: IconThemeData(color: Colors.black),
-            
-          ));
+class MyTheme {
+  static ThemeData lightTheme(BuildContext context) => ThemeData(
+      primarySwatch: Colors.brown,
+      fontFamily: GoogleFonts.poppins().fontFamily,
+      appBarTheme: AppBarTheme(
+        color: Colors.white,
+        elevation: 0.0,
+        textTheme: Theme.of(context).textTheme,
+        iconTheme: IconThemeData(color: Colors.black),
+      ));
 
-static ThemeData  darkTheme(BuildContext context) =>ThemeData(
-           brightness: Brightness.dark,
-           );
+  static ThemeData darkTheme(BuildContext context) => ThemeData(
+        brightness: Brightness.dark,
+      );
 
-
-
+  //colors
+  static Color creamColor = Color(0xfff5f5f5);
+  static Color darkBluishColor = Color(0xff403b58);
+  static Color BrownColor = Color(0xff964B00);
 }
